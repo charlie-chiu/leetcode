26829739f53b039ff37025c60b64e77692d033ce
charlie <marmot00@gmail.com>
2020-08-11 01:28:26 +0800

    1_two_sum add 2 solutions

--- 1_two_sum/solution.go
+++ 1_two_sum/solution.go
@@ -1,6 +1,6 @@
 package two_sum
 
-var solution = bruteForce
+var solution func([]int, int) []int = onePassHashTable
 
 // first try time complexity: O(n^2)
 func bruteForce(nums []int, target int) []int {
@@ -10,6 +10,44 @@ func bruteForce(nums []int, target int) []int {
 				return []int{firstIndex, i2 + firstIndex + 1}
 			}
 		}
+	}
+
+	return nil
+}
+
+//from leetcode solution,
+//time complexity: O(n)
+//space complexity: O(n)
+func twoPassHashTable(nums []int, target int) []int {
+	table := make(map[int]int, len(nums))
+	for i, num := range nums {
+		table[num] = i
+	}
+
+	for i, num := range nums {
+		t := target - num
+
+		if i2, ok := table[t]; ok && i != i2 {
+			return []int{i, i2}
+		}
+	}
+
+	return nil
+}
+
+//from leetcode solution,
+//time complexity: O(n)
+//space complexity: O(n)
+func onePassHashTable(nums []int, target int) []int {
+	table := make(map[int]int, len(nums))
+	for i, num := range nums {
+		complement := target - num
+
+		if i2, ok := table[complement]; ok {
+			return []int{i2, i}
+		}
+
+		table[num] = i
 	}
 
 	return nil
