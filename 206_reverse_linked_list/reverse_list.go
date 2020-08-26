9662fb29cee457f993590837e1e0d5976b441246
Charlie_Chiu <marmot00@gmail.com>
2020-08-26 10:38:32 +0800

    206_reverse_list

--- 206_reverse_linked_list/reverse_list.go
+++ 206_reverse_linked_list/reverse_list.go
@@ -5,8 +5,18 @@ import (
 	"strings"
 )
 
+// first try, O (n)
+// 0ms / 2.6 MB
 func reverseList(head *ListNode) *ListNode {
-	return head
+	var prevNode *ListNode
+	for head != nil {
+		nextNode := head.Next
+		head.Next = prevNode
+		prevNode = head
+		head = nextNode
+	}
+
+	return prevNode
 }
 
 type ListNode struct {
--- 206_reverse_linked_list/reverse_list_test.go
+++ 206_reverse_linked_list/reverse_list_test.go
@@ -12,7 +12,7 @@ type TestCase struct {
 func TestMiddleNode(t *testing.T) {
 	var TestCases = []TestCase{
 		{newLinkedList([]int{1, 2, 3}), newLinkedList([]int{3, 2, 1})},
-		//{newLinkedList([]int{1, 2, 3, 4, 5}), newLinkedList([]int{5, 4, 3, 2, 1})},
+		{newLinkedList([]int{1, 2, 3, 4, 5}), newLinkedList([]int{5, 4, 3, 2, 1})},
 	}
 
 	for _, testCase := range TestCases {
