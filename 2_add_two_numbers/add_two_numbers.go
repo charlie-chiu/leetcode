aa2102103e3d34594d321591645bf3d5fa20a2a7
Charlie_Chiu <marmot00@gmail.com>
2020-08-28 10:17:38 +0800

    2_add_two_numbers

--- 2_add_two_numbers/add_two_numbers.go
+++ 2_add_two_numbers/add_two_numbers.go
@@ -5,5 +5,30 @@ import (
 )
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return l1
+	var valueL1, valueL2, carry int
+	var resHead = &ListNode{}
+	var result = resHead
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 == nil {
+			valueL1 = 0
+		} else {
+			valueL1 = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 == nil {
+			valueL2 = 0
+		} else {
+			valueL2 = l2.Val
+			l2 = l2.Next
+		}
+
+		result.Next = &ListNode{
+			Val: (valueL1 + valueL2 + carry) % 10,
+		}
+		result = result.Next
+		carry = (valueL1 + valueL2 + carry) / 10
+	}
+
+	return resHead.Next
 }
--- 2_add_two_numbers/add_two_numbers_test.go
+++ 2_add_two_numbers/add_two_numbers_test.go
@@ -1,6 +1,7 @@
 package add_two_numbers
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -13,20 +14,23 @@ type TestCase struct {
 
 func TestAddTwoNumbers(t *testing.T) {
 	var TestCases = []TestCase{
+		{NewLinkedList(9, 9), NewLinkedList(1), NewLinkedList(0, 0, 1)},
 		{NewLinkedList(1, 8), NewLinkedList(0), NewLinkedList(1, 8)},
 		{NewLinkedList(5), NewLinkedList(5), NewLinkedList(0, 1)},
 		{NewLinkedList(2, 4, 3), NewLinkedList(5, 6, 4), NewLinkedList(7, 0, 8)},
 	}
 
-	for _, tc := range TestCases {
+	for i, tc := range TestCases {
 		got := addTwoNumbers(tc.L1, tc.L2)
 
-		if !reflect.DeepEqual(got, tc.output) {
-			t.Errorf("wrong ansewr")
-			t.Logf("  L1:%v", tc.L1)
-			t.Logf("  L2:%v", tc.L2)
-			t.Logf("want:%v", tc.output)
-			t.Logf(" got:%v", got)
-		}
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			if !reflect.DeepEqual(got, tc.output) {
+				t.Errorf("wrong ansewr")
+				t.Logf("  L1:%v", tc.L1)
+				t.Logf("  L2:%v", tc.L2)
+				t.Logf("want:%v", tc.output)
+				t.Logf(" got:%v", got)
+			}
+		})
 	}
 }
