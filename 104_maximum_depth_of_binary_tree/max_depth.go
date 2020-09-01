ba54274f22e71d1d5f23c38f93ddf4acc95f4ff9
Charlie_Chiu <marmot00@gmail.com>
2020-09-01 16:48:58 +0800

    104_max_depth with recursion

---
+++ 104_maximum_depth_of_binary_tree/max_depth.go
@@ -0,0 +1,20 @@
+package maximum_depth
+
+import . "leetcode"
+
+// accepted on first submit
+// 4ms / 4.4MB
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
+}
