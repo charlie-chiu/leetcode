0692854c12ecdf9b570ad8e2b0a45568fd153f04
Charlie_Chiu <marmot00@gmail.com>
2020-09-09 09:53:28 +0800

    75_sort_colors (DNF - Dutch national flag)

--- 75_sort_colors/sort_colors.go
+++ 75_sort_colors/sort_colors.go
@@ -1,5 +1,26 @@
 package sort_colors
 
+const (
+	red   = 0
+	white = 1
+	blue  = 2
+)
+
 func sortColors(nums []int) {
-	//
+	//DNF algorithm
+	var L, M, R = 0, 0, len(nums) - 1
+
+	for M <= R {
+		switch nums[M] {
+		case red:
+			nums[L], nums[M] = nums[M], nums[L]
+			L++
+			M++
+		case white:
+			M++
+		case blue:
+			nums[M], nums[R] = nums[R], nums[M]
+			R--
+		}
+	}
 }
