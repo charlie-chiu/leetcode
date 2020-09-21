76b6627497dc46b7ebeddba3db58923149fd1578
Charlie_Chiu <marmot00@gmail.com>
2020-09-21 08:58:41 +0800

    53 maximum subarray - improved brute force

--- 53_maximum_subarray/max_subarray.go
+++ 53_maximum_subarray/max_subarray.go
@@ -1,18 +1,17 @@
 package maximum_subarray
 
-import (
-	"math"
-)
+import "math"
 
-// brute force
-// time complexity O(n^3)
-// got TLE on leetcode
+// improved brute force
+// 84ms (5.96%), 3.3MB on leetcode
 func maxSubArray(nums []int) int {
 	var maxSum int = math.MinInt32
+	var currSum int
 
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j <= len(nums); j++ {
-			currSum := sum(nums[i:j])
+		currSum = 0
+		for j := i; j < len(nums); j++ {
+			currSum += nums[j]
 			if currSum > maxSum {
 				maxSum = currSum
 			}
@@ -22,9 +21,27 @@ func maxSubArray(nums []int) int {
 	return maxSum
 }
 
-func sum(nums []int) (sum int) {
-	for _, num := range nums {
-		sum += num
-	}
-	return
-}
+// brute force
+// time complexity O(n^3)
+// got TLE on leetcode
+//func maxSubArray(nums []int) int {
+//	var maxSum int = math.MinInt32
+//
+//	for i := 0; i < len(nums); i++ {
+//		for j := i + 1; j <= len(nums); j++ {
+//			currSum := sum(nums[i:j])
+//			if currSum > maxSum {
+//				maxSum = currSum
+//			}
+//		}
+//	}
+//
+//	return maxSum
+//}
+//
+//func sum(nums []int) (sum int) {
+//	for _, num := range nums {
+//		sum += num
+//	}
+//	return
+//}
