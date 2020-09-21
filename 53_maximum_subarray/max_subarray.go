989cde8f10de1bc83a93dafa7691b3028a767b2b
Charlie_Chiu <marmot00@gmail.com>
2020-09-21 17:53:58 +0800

    53 maximum subarray - kadane's algorithm (?)

--- 53_maximum_subarray/max_subarray.go
+++ 53_maximum_subarray/max_subarray.go
@@ -2,24 +2,47 @@ package maximum_subarray
 
 import "math"
 
-// improved brute force
-// 84ms (5.96%), 3.3MB on leetcode
+/*
+	time complexity: O(n)
+	is this kadane's algorithm?
+*/
 func maxSubArray(nums []int) int {
 	var maxSum int = math.MinInt32
 	var currSum int
 
-	for i := 0; i < len(nums); i++ {
-		currSum = 0
-		for j := i; j < len(nums); j++ {
-			currSum += nums[j]
-			if currSum > maxSum {
-				maxSum = currSum
-			}
+	for _, num := range nums {
+		if currSum < 0 {
+			currSum = num
+		} else {
+			currSum += num
+		}
+
+		if currSum > maxSum {
+			maxSum = currSum
 		}
 	}
 
 	return maxSum
 }
+
+// improved brute force
+// 84ms (5.96%), 3.3MB on leetcode
+//func maxSubArray(nums []int) int {
+//	var maxSum int = math.MinInt32
+//	var currSum int
+//
+//	for i := 0; i < len(nums); i++ {
+//		currSum = 0
+//		for j := i; j < len(nums); j++ {
+//			currSum += nums[j]
+//			if currSum > maxSum {
+//				maxSum = currSum
+//			}
+//		}
+//	}
+//
+//	return maxSum
+//}
 
 // brute force
 // time complexity O(n^3)
