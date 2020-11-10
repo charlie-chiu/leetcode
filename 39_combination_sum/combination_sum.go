807ea9c90c9c990569182171bca50fc4b71592cd
Charlie_Chiu <marmot00@gmail.com>
2020-11-10 09:33:56 +0800

    39 combination sum - first accepted

--- 39_combination_sum/combination_sum.go
+++ 39_combination_sum/combination_sum.go
@@ -1,5 +1,27 @@
 package combination_sum
 
 func combinationSum(candidates []int, target int) [][]int {
-	return [][]int{}
+	var result [][]int
+
+	helper(candidates, target, []int{}, &result)
+
+	return result
+}
+
+func helper(candidates []int, target int, path []int, result *[][]int) {
+	if target < 0 {
+		return
+	}
+
+	if target == 0 {
+		temp := make([]int, len(path))
+		copy(temp, path)
+
+		*result = append(*result, temp)
+		return
+	}
+
+	for i, candidate := range candidates {
+		helper(candidates[i:], target-candidate, append(path, candidate), result)
+	}
 }
