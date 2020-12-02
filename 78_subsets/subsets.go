1a831f888d92d8a63d64df8d5c0882d0109bc862
Charlie_Chiu <marmot00@gmail.com>
2020-12-02 11:25:03 +0800

    78 subsets - add recurse approach

--- 78_subsets/subsets.go
+++ 78_subsets/subsets.go
@@ -1,36 +1,36 @@
 package subsets
 
 // iterative approach
-func subsets(nums []int) [][]int {
-	var subsets = [][]int{{}}
-
-	for _, num := range nums {
-		for _, subset := range subsets {
-			newSubset := make([]int, len(subset), len(subset)+1)
-			copy(newSubset, subset)
-			newSubset = append(newSubset, num)
-			subsets = append(subsets, newSubset)
-		}
-	}
-
-	return subsets
-}
-
-// recurse approach
 //func subsets(nums []int) [][]int {
-//	return subsetsHelper([]int{}, nums)
-//}
+//	var subsets = [][]int{{}}
 //
-//func subsetsHelper(path []int, nums []int) [][]int {
-//	//base case, no more num to choose
-//	if len(nums) == 0 {
-//		return [][]int{path}
+//	for _, num := range nums {
+//		for _, subset := range subsets {
+//			newSubset := make([]int, len(subset), len(subset)+1)
+//			copy(newSubset, subset)
+//			newSubset = append(newSubset, num)
+//			subsets = append(subsets, newSubset)
+//		}
 //	}
 //
-//	firstNum := nums[0]
-//	nums = nums[1:]
-//	var choosePath = make([]int, len(path))
-//	copy(choosePath, path)
-//	// choose first number                             //not choose first number
-//	return append(subsetsHelper(append(choosePath, firstNum), nums), subsetsHelper(path, nums)...)
+//	return subsets
 //}
+
+// recurse approach
+func subsets(nums []int) [][]int {
+	return subsetsHelper([]int{}, nums)
+}
+
+func subsetsHelper(path []int, nums []int) [][]int {
+	//base case, no more num to choose
+	if len(nums) == 0 {
+		return [][]int{path}
+	}
+
+	firstNum := nums[0]
+	nums = nums[1:]
+	var choosePath = make([]int, len(path))
+	copy(choosePath, path)
+	// choose first number                             //not choose first number
+	return append(subsetsHelper(append(choosePath, firstNum), nums), subsetsHelper(path, nums)...)
+}
